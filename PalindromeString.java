42ab80dac51c131593322293f702c2e3c9dc4184
shivanjali14240 <148749643+shivanjali14240@users.noreply.github.com>
2023-11-14 20:26:40 +0530

    Add files via upload

---
+++ PalindromeString.java
@@ -0,0 +1,21 @@
+package com.bnt;
+
+public class PalindromeString {
+	public static void main(String[] args) {
+		System.out.println(isPalindrome("app"));
+	}
+
+	public static int isPalindrome(String s) {
+		StringBuilder revsString = new StringBuilder();
+		char[] c = s.toCharArray();
+		for (int i = c.length - 1; i >= 0; i--) {
+			revsString = revsString.append(c[i]);
+		}
+		if (s.contains(revsString)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+}
